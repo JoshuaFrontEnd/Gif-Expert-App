c66b19418daa98974fb48da0a6aa5b52c450b34a
Joshua <hola@joshua.cl>
2023-06-08 14:50:20 -0400

    Creando una lista de categorías, primeros apuntes

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,4 +1,15 @@
+// Cuando se llame un Hook especifico de React, se debe de importar desde la libreria
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+
+  // Usando el Hook "useState" de React (esto es basicamente una variable de estado)
+  // Cuando llamamos al useState, le estamos diciendo a React que debe recordar algo
+  // "categories" es una variable de estado y "setCategories" es la función que setea el estado
+  const [ categories, setCategories ] = useState(['One Punch', 'Dragón Ball']);
+
+  console.log( categories );
+
   return (
     <>
       {/* Titulo */}
@@ -7,6 +18,26 @@ export const GifExpertApp = () => {
       {/* Input */}
 
       {/* Listado de Gif's */}
+      <ol>
+        {/* Cuando se utilizan metodos para recorrer arreglos (como "map" por ejemplo) React nos pide asignar obligatoriamente un atributo de nombre "key" de valor unico al elemento que será iterado, de esta manera React puede identificar que elementos han cambiado de manera dinamica
+
+        Más información acá: https://www.escuelafrontend.com/prop-key-en-react
+
+        */}
+        { categories.map( category => <li key={ category }>{ category }</li> )}
+      </ol>
     </>
   )
 }
+
+
+/* ----------------------------------------------------------------
+  Apuntes adicionales
+
+  - Cuando haya que almacenar información y esa información tiene que cambiar el HTML, se debe utilizar un Hook de React para mantener el estado
+
+  Más información acá: https://es.react.dev/learn/state-a-components-memory
+
+  - Cuando se setean multiples variables de estado React para identificarlas crea un array con cada variable, de esta manera puede identificarlas mediante su indice, debido a esto, no se deben setear variables de estado adentro de funciones o ciclos iterativos
+---------------------------------------------------------------- */
+
